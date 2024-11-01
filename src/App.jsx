bfda84be7d4873baacbb013fa71c82b94f8600af
Mcheaven <115719615+FransManlangit@users.noreply.github.com>
2024-11-01 18:11:49 +0800

    add toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,42 @@
 import React, { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";// Import BrowserRouter
+import { Routes, Route } from "react-router-dom";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "./App.css";
+import { Toaster } from 'react-hot-toast';
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 import { useDispatch, useSelector } from "react-redux";
-import store from "./store"
+import store from "./store";
 import { LoadUser } from "./actions/userActions";
-
-
 
 import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
-
 import SignUp from "./pages/SignUp/SignUp";
 import Login from "./pages/Login/Login";
 import Profile from "./pages/Profile/Profile";
 import SuccessVerify from "./pages/VerifyEmail/VerifyEmail";
-
 import Products from "./pages/Product/Product";
 
-
-
 function App() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
+    store.dispatch(LoadUser());
+  }, []);
 
-
-    store.dispatch(LoadUser())
-  }, [])
   return (
     <div>
+      {/* Toaster component to display toast notifications */}
+      <Toaster position="top-center" reverseOrder={false} />
+
       <Routes>
         <Route path="/" element={<Home />} exact="true" />
         <Route path='/verify/email/:token/:id' element={<SuccessVerify />} exact='true' />
         <Route path='/signup' element={<SignUp />} exact='true' />
         <Route path='/login' element={<Login />} exact='true' />
         <Route path='/userprofile' element={<ProtectedRoute><Profile /></ProtectedRoute>} exact='true' />
-        <Route
-          path="/products"
-          element={
-            <Products />
-          }
-        />
-
+        <Route path="/products" element={<Products />} />
       </Routes>
     </div>
   );
