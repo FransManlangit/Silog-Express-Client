e28aa349fb1baaf860edc4dc4859f5af7bcb3fc8
Mcheaven <115719615+FransManlangit@users.noreply.github.com>
2024-11-01 18:12:09 +0800

    fix user actions

--- src/actions/userActions.jsx
+++ src/actions/userActions.jsx
@@ -14,7 +14,17 @@ import {
   LOGIN_REQUEST,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  
+
+  UPDATE_PROFILE_REQUEST,
+  UPDATE_PROFILE_SUCCESS,
+  UPDATE_PROFILE_RESET,
+  UPDATE_PROFILE_FAIL,
+
+  UPDATE_USER_REQUEST,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_RESET,
+  UPDATE_USER_FAIL,
+
 
   LOAD_USER_REQUEST,
   LOAD_USER_SUCCESS,
@@ -79,62 +89,84 @@ export const verifyUserEmail = (token, id) => async (dispatch) => {
 
 export const LoginUsers = (email, password) => async (dispatch) => {
   try {
-      dispatch({
-          type: LOGIN_REQUEST
-      })
-      const config = {
-          headers: {
-              "Content-Type": "application/json",
-          },
-          withCredentials: true
-      }
-      const { data } = await axios.post(`${import.meta.env.VITE_APP_API
+    dispatch({
+      type: LOGIN_REQUEST
+    })
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      withCredentials: true
+    }
+    const { data } = await axios.post(`${import.meta.env.VITE_APP_API
       }/api/v1/login`, { email, password }, config);
-      console.log('Login Response:', data);
-      dispatch({
-          type: LOGIN_SUCCESS,
-          payload: data.user
-      });
+    console.log('Login Response:', data);
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: data.user
+    });
   } catch (error) {
-      dispatch({
-          type: LOGIN_FAIL,
-          payload: error.response.data.message
-      })
+    dispatch({
+      type: LOGIN_FAIL,
+      payload: error.response.data.message
+    })
   }
 }
 
 export const LoadUser = () => async (dispatch) => {
   try {
-      dispatch({
-          type: LOAD_USER_REQUEST
-      })
-      const { data } = await axios.get(`${import.meta.env.VITE_APP_API
+    dispatch({
+      type: LOAD_USER_REQUEST
+    })
+    const { data } = await axios.get(`${import.meta.env.VITE_APP_API
       }/api/v1/userprofile`, { withCredentials: true })
-      dispatch({
-          type: LOAD_USER_SUCCESS,
-          payload: data.user
-      })
+    dispatch({
+      type: LOAD_USER_SUCCESS,
+      payload: data.user
+    })
   } catch (error) {
-      dispatch({
-          type: LOAD_USER_FAIL,
-          payload: error.response.data.message
-      })
+    dispatch({
+      type: LOAD_USER_FAIL,
+      payload: error.response.data.message
+    })
   }
-  
+
 }
 
 export const Logout = () => async (dispatch) => {
   try {
-      await axios.get(`${import.meta.env.VITE_APP_API
+    await axios.get(`${import.meta.env.VITE_APP_API
       }/api/v1/logout`, { withCredentials: true });
+    dispatch({
+      type: LOGOUT_SUCCESS
+    })
+  } catch (error) {
+    dispatch({
+      type: LOGIN_FAIL,
+      payload: error.response.data.message
+    })
+  }
+}
+
+export const updateProfile = (userData) => async (dispatch) => {
+  try {
+      dispatch({ type: UPDATE_PROFILE_REQUEST })
+      const config = {
+          headers: {
+              "Content-Type": "multipart/form-data",
+          },
+          withCredentials: true
+      }
+      const { data } = await axios.put(`${import.meta.env.VITE_APP_API}/api/v1/updateprofile`, userData, config)
       dispatch({
-          type: LOGOUT_SUCCESS
+          type: UPDATE_PROFILE_SUCCESS,
+          payload: data.success,
       })
   } catch (error) {
       dispatch({
-          type: LOGIN_FAIL,
-          payload: error.response.data.message
-      })
+          type: UPDATE_PROFILE_FAIL,
+          payload: error.response.data.message,
+      });
   }
 }
 
