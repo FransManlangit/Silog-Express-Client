1b85c7781ece44eca7529aefa51059534072fae5
Mcheaven <115719615+FransManlangit@users.noreply.github.com>
2024-11-01 18:41:12 +0800

    enhance profile

--- src/components/ProfileSidebar/ProfileSidebar.jsx
+++ src/components/ProfileSidebar/ProfileSidebar.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { Avatar, Button, Divider, Image } from "antd";
 import { SlUser } from "react-icons/sl";
 
+
 export default function ProfileSidebar() {
   const { user, loading, isLogout } = useSelector((state) => state.authUser);
 
@@ -26,7 +27,7 @@ export default function ProfileSidebar() {
 
               <div className="flex flex-col items-center">
                 <span className="font-extrabold text-normal tracking-normal text-xl">
-                  <span className="text-[#1E4BCA]">
+                  <span className="text-[#D56F00]">
                     {user.firstname}
                   </span>
                 </span>
@@ -37,21 +38,21 @@ export default function ProfileSidebar() {
             <Divider />
 
             <div className="space-y-4">
-              <div className="hover:bg-[#1E4BCA] hover:text-white py-2 px-3 rounded cursor-pointer w-full flex flex-1 flex-row justify-between items-center">
+              <div className="hover:bg-[#D56F00] hover:text-white py-2 px-3 rounded cursor-pointer w-full flex flex-1 flex-row justify-between items-center">
                 <div className="flex items-center gap-4">
-                  <SlUser color="#0f172a" size={20} />
+                <i className="pi pi-user-edit" style={{ color: '#0f172a', fontSize: '25px' }} />
 
                   <span className="">Profile</span>
                 </div>
               </div>
-{/* 
-              <div className="hover:bg-[#1E4BCA] hover:text-white py-2 px-3 rounded cursor-pointer w-full flex flex-1 flex-row justify-between items-center">
+
+              <div className="hover:bg-[#D56F00] hover:text-white py-2 px-3 rounded cursor-pointer w-full flex flex-1 flex-row justify-between items-center">
                 <div className="flex items-center gap-4">
-                  <LiaTruckLoadingSolid color="#0f172a" size={20} />
+                  <i className="pi pi-shopping-cart" style={{ color: '#0f172a', fontSize: '25px' }}/>
 
                   <span className="">My Orders</span>
                 </div>
-              </div> */}
+              </div>
             </div>
 
             {/* <Divider /> */}
